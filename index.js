c12466f8a87f95b95a9cfe4ea74d6a598ecc8d56
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-04-28 23:56:59 +0600

    post methode for add new spot implement

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5000
 const app = express()
-// middelware
 
+// middelware
+app.use(express.json())
+app.use(cors())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAE}:${process.env.DB_PASSWORD}@cluster0.pbmq8lu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,22 +24,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const Spots = client.db("Spots").collection("mySpots");
+
+        app.post("/addSpots", async (req,res)=> {
+            const spots = req.body;
+            const result = await Spots.insertOne(spots)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-//
-//
-
-app.use(express())
-app.use(cors())
 
 app.get("/", (req,res) => {
     res.send("first server running")
